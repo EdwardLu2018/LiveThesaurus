9c215ecc5c4e98c394bab1bf804c95b83527aa25
Edward <lued2018@gmail.com>
2018-11-21 17:52:50 -0500

    Update graphics.py

--- graphics.py
+++ graphics.py
@@ -22,7 +22,7 @@ class LiveThesaurus(object):
         self.currentDefList = [None]
         self.currentDef = None
         self.currentDefIndex = 0
-                
+        
         # CITATION: Code from: https://stackoverflow.com/questions/15981000/tkinter-python-maximize-window
         # makes the application window full screen
         screenWidth = master.winfo_screenwidth()
@@ -30,9 +30,8 @@ class LiveThesaurus(object):
         self.master.geometry("%dx%d+0+0" % (screenWidth, screenHeight))
         self.master.config(background='black')
         
+        ## Left Frame
         self.leftFrame = Frame(self.master)
-        # creates a text box and scroll bar, along with a button that will
-        # print synonyms on the right side of the window
         self.textScrollBar = Scrollbar(self.leftFrame)
         self.instructionsLabel = Label(self.leftFrame, 
                         text="Welcome to LiveThesaurus!\n" +
@@ -53,16 +52,15 @@ class LiveThesaurus(object):
         self.textBox.pack(side=TOP, fill=BOTH, padx=3, pady=3)
         self.audioButton.pack(side=TOP, fill=BOTH)
         
-        
-        # creates the right frame of the window
+        ## Right Frame
         self.rightFrame = Frame(self.master)
         self.wordAndDefFrame = Frame(self.rightFrame)
         self.wordInfoFrame = Frame(self.wordAndDefFrame, borderwidth=1, relief="solid")
         self.defInfoFrame = Frame(self.wordAndDefFrame, borderwidth=1, relief="solid")
         self.innerDefFrame = Frame(self.defInfoFrame)
-        self.synFrame = Frame(self.rightFrame)
-        self.innerSynFrame = Frame(self.synFrame, borderwidth=1, relief="solid")
-        self.modeFrame = Frame(self.innerSynFrame)
+        self.termFrame = Frame(self.rightFrame)
+        self.innerTermFrame = Frame(self.termFrame, borderwidth=1, relief="solid")
+        self.modeFrame = Frame(self.innerTermFrame)
         
         # labels for the selected word, current definition, and synonyms
         self.currentWordLabel = Label(self.wordInfoFrame, 
@@ -70,14 +68,15 @@ class LiveThesaurus(object):
                             anchor=N)
         self.definitionLabel = Label(self.innerDefFrame, text="Definition: ", 
                                      anchor=N)
-        self.synInstructionsLabel = Label(self.synFrame, 
+        self.synInstructionsLabel = Label(self.termFrame, 
                 text="Pick a synonym and press the Enter key to change the word",
                 borderwidth=1, relief="solid", anchor=N)
         self.synonymTitle = Label(self.modeFrame, text="List", anchor=N)
         self.toggleSynOrAntButton = Button(self.modeFrame, width=8, height=1, 
                              text="Synonyms", 
                              command=self.switchModes)
-        self.termBox = Listbox(self.synFrame, borderwidth=2, relief="solid")
+        self.termBox = Listbox(self.termFrame, borderwidth=2, relief="solid")
+        self.termScrollBar = Scrollbar(self.termBox)
         
         # CITATION: Option Menu Code from: https://stackoverflow.com/questions/35132221/tkinter-optionmenu-how-to-get-the-selected-choice
         # creates and packs an option menu for definitions
@@ -88,9 +87,8 @@ class LiveThesaurus(object):
         
         self.rightFrame.config(background="black")
         self.definitionMenu.config(width=35)
-        self.synScrollBar = Scrollbar(self.termBox)
-        self.synScrollBar.config(command=self.termBox.yview)
-        self.termBox.config(yscrollcommand=self.synScrollBar.set)
+        self.termScrollBar.config(command=self.termBox.yview)
+        self.termBox.config(yscrollcommand=self.termScrollBar.set)
         self.termBox.bind("<<ListboxSelect>>", self.updateCurrentSynOrAnt)
         self.termBox.bind("<Return>", self.replaceWordWithSynOrAnt)
         
@@ -103,14 +101,14 @@ class LiveThesaurus(object):
         self.innerDefFrame.pack(side=TOP, padx=2, pady=2)
         self.definitionLabel.pack(side=LEFT, fill=BOTH, pady=2)
         self.definitionMenu.pack(side=LEFT, fill=BOTH)
-        self.synFrame.pack(side=TOP, fill=BOTH, expand=YES, padx=3, pady=3)
+        self.termFrame.pack(side=TOP, fill=BOTH, expand=YES, padx=3, pady=3)
         self.synInstructionsLabel.pack(side=TOP, fill=BOTH, padx=3, pady=3)
-        self.innerSynFrame.pack(side=TOP, fill=BOTH, padx=3, pady=3)
+        self.innerTermFrame.pack(side=TOP, fill=BOTH, padx=3, pady=3)
         self.modeFrame.pack(side=TOP, padx=2, pady=2)
         self.synonymTitle.pack(side=LEFT, padx=2, pady=2)
         self.toggleSynOrAntButton.pack(side=LEFT, padx=2, pady=2)
         self.termBox.pack(side=TOP, fill=BOTH, expand=YES, padx=2)
-        self.synScrollBar.pack(side=RIGHT, fill=Y)
+        self.termScrollBar.pack(side=RIGHT, fill=Y)
 
         self.generateSynOrAntList()
         self.timerFiredWrapper()
