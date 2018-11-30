bd2c9266b4435b51ba12e00ab089c129a3ddd958
Edward <lued2018@gmail.com>
2018-11-30 00:54:52 -0500

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from tkinter import *
 import LiveThesaurus as application
 
-# creates a LiveThesaurus application and runs application
+# creates a LiveThesaurus application and runs the application
 root = Tk()
 application = application.LiveThesaurus(root)
 application.run()
